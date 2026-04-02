7a3bb1c04b2177d13345d674a24439c1def33e89
rafaelandrade130310-dot <rafaelandrade130310@gmail.com>
2026-04-02 16:10:23 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App simples',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const TelaInicial(),
+    );
+  }
+}
+
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({super.key});
+
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  String texto = "Clique no botão";
+
+  void mudarTexto() {
+    setState(() {
+      texto = "Você clicou!";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF2F3F7),
+      appBar: AppBar(title: const Text("Tela inicial"), elevation: 0),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: const [
+              BoxShadow(
+                blurRadius: 8,
+                color: Colors.black12,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(texto, style: const TextStyle(fontSize: 18)),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: mudarTexto,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 3, 3, 3),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 12,
+                  ),
+                ),
+                child: const Text("Aperta aqui"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
